16dd0cd4d504d184ecd57cd730c5a747c4f386ee
Martin Schlegel <schlegel@itestra.de>
2015-03-03 08:47:57 +0100

    Regression test

---
+++ regTest/runTests.bat
@@ -0,0 +1,24 @@
+@ del *.log.1
+@ cd ..
+@ mnh_console demos\99bottles.mnh        > regTest\99bottles.log.1
+@ mnh_console demos\anagram.mnh          > regTest\anagram.log.1
+@ mnh_console demos\digitSquareSum.mnh   > regTest\digitSquareSum.log.1
+@ mnh_console demos\fibonacci.mnh        > regTest\fibonacci.log.1
+@ mnh_console demos\fibonacciWord.mnh    > regTest\fibonacciWord.log.1
+@ mnh_console demos\hailstone.mnh        > regTest\hailstone.log.1
+@ mnh_console demos\hofstadterConway.mnh > regTest\hofstadterConway.log.1
+@ mnh_console demos\huffmann.mnh         > regTest\huffmann.log.1
+@ mnh_console demos\josephus.mnh         > regTest\josephus.log.1
+@ mnh_console demos\quine.mnh            > regTest\quine.log.1
+@ mnh_console demos\quine2.mnh           > regTest\quine2.log.1
+@ mnh_console demos\rk4.mnh              > regTest\rk4.log.1
+@ mnh_console demos\root.mnh             > regTest\root.log.1
+@ mnh_console demos\setConsolidation.mnh > regTest\setConsolidation.log.1
+@ mnh_console demos\shannonEntropy.mnh   > regTest\shannonEntropy.log.1
+@ mnh_console demos\sierpinski.mnh       > regTest\sierpinski.log.1
+@ mnh_console demos\ulam.mnh             > regTest\ulam.log.1
+@ mnh_console demos\vampire.mnh          > regTest\vampire.log.1
+@ mnh_console demos\wordCount.mnh        > regTest\wordCount.log.1
+@ mnh_console demos\y.mnh                > regTest\y.log.1
+@ mnh_console regTest\compareLogs.mnh
+@ cd regTest
