a7dc09e8453178d8c77b74d1829ba98adb4cdb5f
Martin Schlegel <schlegel@itestra.de>
2015-03-11 09:09:09 +0100

    New demo/testcase zeckendorf.mnh

--- doTests.bat
+++ doTests.bat
@@ -1,2 +1,2 @@
-mnh_console.exe regTest\test.mnh
-mnh_console.exe regTest\test.mnh bin32\mnh_console.exe
+mnh_console.exe regTest\test.mnh mnh_console.exe %1
+mnh_console.exe regTest\test.mnh bin32\mnh_console.exe %1
