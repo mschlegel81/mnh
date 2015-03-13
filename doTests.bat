463e941fc9d1f34b40c904756a3b68afee6f65d3
Martin Schlegel <schlegel@itestra.de>
2015-03-13 14:49:58 +0100

    Complex (p)each aggregators can/must be given as expressions - demos/test-cases

--- doTests.bat
+++ doTests.bat
@@ -1,2 +1,2 @@
-mnh_console.exe regTest\test.mnh mnh_console.exe %1
-mnh_console.exe regTest\test.mnh bin32\mnh_console.exe %1
+mnh_console.exe regTest\test.mnh mnh_console.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+mnh_console.exe regTest\test.mnh bin32\mnh_console.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
