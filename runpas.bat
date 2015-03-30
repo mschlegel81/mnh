80b8fe410b8e00b124751c390c34621de1e797ae
Martin Schlegel <schlegel@itestra.de>
2015-03-30 15:55:00 +0200

    Fixed runpas.bat

--- runpas.bat
+++ runpas.bat
@@ -2,8 +2,7 @@
 @goto run
 
 :cleanup
-  @c:\lazarus\fpc\2.6.4\bin\i386-win32\delp . core util test consoles bin
-  @del test\*.exe consoles\*.exe bin\*.exe gui\*.exe gui\*.gdb
+  @c:\lazarus\fpc\2.6.4\bin\i386-win32\delp . core util test consoles bin gui 
   @goto end
 
 :run
