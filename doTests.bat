f8fb764810455801594904a2630e153c2446ea76
Martin Schlegel <schlegel@itestra.de>
2015-05-08 13:31:54 +0200

    Restored test-compatibility of 32bit and 64bit versions

--- doTests.bat
+++ doTests.bat
@@ -1,4 +1,4 @@
-mnh.exe regTest\test.mnh mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-mnh.exe regTest\test.mnh bin32\mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-mnh.exe regTest\test.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-mnh.exe regTest\test.mnh bin32\mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh.exe regTest\test.mnh mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh.exe regTest\test.mnh bin32\mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh.exe regTest\test.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh.exe regTest\test.mnh bin32\mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
