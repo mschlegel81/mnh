241711fb1982d2275525171969dbe8092492115e
Martin Schlegel <schlegel@itestra.de>
2015-05-11 09:35:37 +0200

    doTests.bat makes a distro-package when all tests are passed

--- doTests.bat
+++ doTests.bat
@@ -1,4 +1,10 @@
 @mnh.exe regTest\test.mnh mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@if errorlevel 1 goto end 
 @mnh.exe regTest\test.mnh bin32\mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@if not errorlevel 0 goto end 
 @mnh.exe regTest\test.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@if not errorlevel 0 goto end 
 @mnh.exe regTest\test.mnh bin32\mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@if not errorlevel 0 goto end 
+@if "%1"=="" call make distro
+:end
