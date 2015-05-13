29cc36baca0706f04f3f1cb941b72143b11b0027
Martin Schlegel <schlegel@itestra.de>
2015-05-13 09:00:13 +0200

    Fixed doTests.bat

--- doTests.bat
+++ doTests.bat
@@ -1,10 +1,10 @@
 @mnh.exe regTest\test.mnh mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 @if errorlevel 2 goto end 
-@mnh.exe regTest\test.mnh bin32\mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@bin32\mnh.exe regTest\test.mnh bin32\mnh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 @if errorlevel 2 goto end 
-@mnh.exe regTest\test.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh_light.exe regTest\test.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 @if errorlevel 2 goto end 
-@mnh.exe regTest\test.mnh bin32\mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@bin32\mnh_light.exe regTest\test.mnh bin32\mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 @if errorlevel 1 goto end 
 @if "%1"=="" call make distro
 :end
