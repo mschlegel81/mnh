a6791738e22352515372e8e9fbf71e8bc655a312
Martin Schlegel <schlegel@itestra.de>
2015-09-02 03:07:32 +0200

    New test (before implementing)

--- runpas.bat
+++ runpas.bat
@@ -1,17 +1,21 @@
+@set delp=C:\lazarus32\fpc\2.6.4\bin\i386-win32\delp 
+@set fpc=C:\lazarus32\fpc\2.6.4\bin\i386-win32\fpc
+
 @if "%1"=="clean" @goto cleanup
 @goto run
 
+
 :cleanup
-  @c:\lazarus\fpc\2.6.4\bin\i386-win32\delp . core util test consoles bin gui 
+  @%delp% c:\lazarus\fpc\2.6.4\bin\i386-win32\delp . core util test consoles bin gui 
   @goto end
 
 :run
   set path=c:\lazarus\fpc\2.6.4\bin\i386-win32\
   @For /f "tokens=1 delims=\." %%a in ('echo %1') do @del %%a.exe
 
-  @For /f "tokens=1 delims=." %%a in ('echo %1') do  c:\lazarus\fpc\2.6.4\bin\i386-win32\fpc %%a.pas -S2 -gl -Fucore -Fuutil -Fu..\core -Fu..\util -Filib\i386-win32 -FuC:\lazarus\components\synedit\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fugui -l -dLCL -dLCLwin32
+  @For /f "tokens=1 delims=." %%a in ('echo %1') do %fpc% %%a.pas -S2 -gl -Fucore -Fuutil -Fu..\core -Fu..\util -Filib\i386-win32 -FuC:\lazarus\components\synedit\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fugui -l -dLCL -dLCLwin32
 
-  @rem @For /f "tokens=1 delims=." %%a in ('echo %1') do c:\lazarus\fpc\2.6.4\bin\i386-win32\fpc %%a.pas -Sh2 -O2 -Fucore -Fuutil ^
+  @rem @For /f "tokens=1 delims=." %%a in ('echo %1') do %fpc% %%a.pas -Sh2 -O2 -Fucore -Fuutil ^
   @rem   -MObjFPC -Scghi -Filib\i386-win32 -FuC:\lazarus\components\synedit\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fugui -l -dLCL -dLCLwin32
   @For /f "tokens=1 delims=." %%a in ('echo %1') do %%a.exe %2 %3 %4 %5 %6 %7 %8 %9
 :end
---
+++ test/listdrives.pas
@@ -0,0 +1,81 @@
+program listdevices;
+ 
+{$ifdef fpc}{$mode delphi}{$endif}
+{$apptype console}
+ 
+uses
+  Windows;
+ 
+FUNCTION getVolumeLabel(CONST DriveChar: Char): string;
+  VAR
+    NotUsed:     DWORD;
+    VolumeFlags: DWORD;
+    VolumeInfo:  array[0..MAX_PATH] of Char;
+    VolumeSerialNumber: DWORD;
+    Buf: array [0..MAX_PATH] of Char;
+  begin
+      GetVolumeInformation(PChar(DriveChar + ':\'),
+      Buf, SizeOf(VolumeInfo), @VolumeSerialNumber, NotUsed,
+      VolumeFlags, nil, 0);
+   
+      writeln(VolumeSerialNumber,' ',VolumeFlags);
+      SetString(Result, Buf, StrLen(Buf));   
+  end;
+
+FUNCTION getVolumeType(CONST drive:char):string;
+  VAR DriveLetter: string;
+  begin
+    DriveLetter := Drive + ':\';
+    case GetDriveType(PChar(DriveLetter)) of
+      DRIVE_REMOVABLE: result:='removable';
+      DRIVE_FIXED:     result:='fixed';
+      DRIVE_REMOTE:    result:='network';
+      DRIVE_CDROM:     result:='CD_ROM';
+      DRIVE_RAMDISK:   result:='RAM_disk';
+      else result:='';
+    end;
+  end;
+  
+  
+var
+  Drive: Char;
+  
+  OldMode: Word; 
+begin
+  writeln(DRIVE_REMOVABLE);
+  writeln(DRIVE_FIXED);
+  writeln(DRIVE_REMOTE);
+  writeln(DRIVE_CDROM);
+  writeln(DRIVE_RAMDISK);
+
+
+  WriteLn('The following drives were found in this computer:');
+  WriteLn('');
+ 
+  // Empty Floppy or Zip drives can generate a Windows error.
+  // We disable system errors during the listing.
+  // Note that another way to skip these errors would be to use DEVICE_IO_CONTROL.
+  OldMode := SetErrorMode(SEM_FAILCRITICALERRORS);
+  try 
+ 
+    // Search all drive letters
+    for Drive := 'A' to 'Z' do
+    begin
+      writeln(drive,' ',getVolumeType(drive),' "',getVolumeLabel(drive),'"');
+//    
+//      DriveLetter := Drive + ':\';
+// 
+//      case GetDriveType(PChar(DriveLetter)) of
+//       DRIVE_REMOVABLE: WriteLn(DriveLetter, ' removable "', getVolumeLabel(Drive),'"');
+//       DRIVE_FIXED:     WriteLn(DriveLetter, ' fixed     "', getVolumeLabel(Drive),'"');
+//       DRIVE_REMOTE:    WriteLn(DriveLetter, ' network   "', getVolumeLabel(Drive),'"');
+//       DRIVE_CDROM:     WriteLn(DriveLetter, ' CD-ROM    "', getVolumeLabel(Drive),'"');
+//       DRIVE_RAMDISK:   WriteLn(DriveLetter, ' RAM disk  "', getVolumeLabel(Drive),'"');
+//      end;
+    end;
+ 
+  finally
+    // Restores previous Windows error mode.
+    SetErrorMode(OldMode); 
+  end;
+end.
