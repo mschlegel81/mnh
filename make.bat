031608a27f5f0b1d058affd8b6ca320965cabdf5
Martin Schlegel <schlegel@itestra.de>
2015-09-17 11:18:10 +0200

    Improved make process: skip unnecessary builds, write setup.mnh if nonexistent

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @if not exist mnh_light.exe (
   C:\lazarus64\fpc\2.6.4\bin\x86_64-win64\fpc.exe consoles\mnh_console.pas -o.\mnh_light.exe -O3 -CX -XX -Si -FuC:\lazarus64\components\synedit\units\x86_64-win64\win32 -FuC:\lazarus64\lcl\units\x86_64-win64\win32 -FuC:\lazarus64\lcl\units\x86_64-win64 -FuC:\lazarus64\components\lazutils\lib\x86_64-win64 -FuC:\lazarus64\packager\units\x86_64-win64 -Fugui -l -dLCL -dLCLwin32 -MObjFPC -Scgh -Fucore -Fuutil -Ficore -dversion64bit
   C:\lazarus64\fpc\2.6.4\bin\x86_64-win64\delp . core util test consoles bin32 gui\lib\i386-win32 gui\lib\x86_64-win64 gui)
-move mnh_light.exe mnh_temp.exe
+copy mnh_light.exe mnh_temp.exe
 mnh_temp.exe make.mnh %*
 @if not exist mnh_light.exe (
   @if "%1"=="clean" (del mnh_temp.exe) else (move mnh_temp.exe mnh_light.exe)
