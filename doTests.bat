3d91a202aeed666812e099608786f7a4b705f154
Martin Schlegel <schlegel@itestra.de>
2015-09-17 12:31:02 +0200

    Hugely improved checkstyle

--- doTests.bat
+++ doTests.bat
@@ -1,8 +1,2 @@
-@mnh.exe                   regTest\regTest.mnh mnh.exe                   %1 %2 %3 %4 %5 %6 %7 %8 %9
-@bin32\mnh.exe             regTest\regTest.mnh bin32\mnh.exe             %1 %2 %3 %4 %5 %6 %7 %8 %9
-@mnh_light.exe             regTest\regTest.mnh mnh_light.exe             %1 %2 %3 %4 %5 %6 %7 %8 %9
-@bin32\mnh_light.exe       regTest\regTest.mnh bin32\mnh_light.exe       %1 %2 %3 %4 %5 %6 %7 %8 %9
-@mnh_debug.exe             regTest\regTest.mnh mnh_debug.exe             %1 %2 %3 %4 %5 %6 %7 %8 %9
-@bin32\mnh_debug.exe       regTest\regTest.mnh bin32\mnh_debug.exe       %1 %2 %3 %4 %5 %6 %7 %8 %9
-@mnh_light_debug.exe       regTest\regTest.mnh mnh_light_debug.exe       %1 %2 %3 %4 %5 %6 %7 %8 %9
-@bin32\mnh_light_debug.exe regTest\regTest.mnh bin32\mnh_light_debug.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh.exe       regTest\regTest.mnh mnh.exe       %1 %2 %3 %4 %5 %6 %7 %8 %9
+@mnh_light.exe regTest\regTest.mnh mnh_light.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
