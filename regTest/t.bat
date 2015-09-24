290af3e1201ab0f290680047e9bc08b5f0a0e8d9
Martin Schlegel <schlegel@itestra.de>
2015-09-24 17:45:25 +0200

    Modified package/test case enhanceBat.mnh following removal of //!BATCH MODE feature

--- regTest/t.bat
+++ regTest/t.bat
@@ -1,4 +1,4 @@
-@rem mnh printf@("@echo %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3%"|each(i,[1..10],i*[1..10]));
+@rem mnh format@("@echo %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3%"|each(i,[1..10],i*[1..10]))
 @echo   1   2   3   4   5   6   7   8   9  10
 @echo   2   4   6   8  10  12  14  16  18  20
 @echo   3   6   9  12  15  18  21  24  27  30
@@ -9,8 +9,7 @@
 @echo   8  16  24  32  40  48  56  64  72  80
 @echo   9  18  27  36  45  54  63  72  81  90
 @echo  10  20  30  40  50  60  70  80  90 100
-@rem mnh printf@("@echo %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3%"|each(i,[1..10],i+[1..10]));
-@rem mnh 2*3;
+@rem mnh format@("@echo %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3% %I3%"|each(i,[1..10],i+[1..10]))
 @echo   2   3   4   5   6   7   8   9  10  11
 @echo   3   4   5   6   7   8   9  10  11  12
 @echo   4   5   6   7   8   9  10  11  12  13
@@ -21,7 +20,9 @@
 @echo   9  10  11  12  13  14  15  16  17  18
 @echo  10  11  12  13  14  15  16  17  18  19
 @echo  11  12  13  14  15  16  17  18  19  20
+@rem mnh 2*3
+6
 @rem nomnh
 @echo this is the old rest
-@rem mnh print("This is the new rest");
+@rem mnh "This is the new rest"
 This is the new rest
