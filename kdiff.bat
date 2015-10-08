f85653bedcd7c9b7477aac570b6c7519184d8230
Martin Schlegel <schlegel@itestra.de>
2015-10-08 13:01:27 +0200

    Enhanced kdiff.bat

--- kdiff.bat
+++ kdiff.bat
@@ -1,1 +1,6 @@
+if "%1"=="" goto diffAll
 "c:\Program Files\KDiff3\kdiff3.exe" %1 %2 
+goto end
+:diffAll
+@For /f "tokens=1 delims=." %%a in ('dir /B regTest\*.last') do @call %0 regTest\%%a.expected regTest\%%a.last
+:end
