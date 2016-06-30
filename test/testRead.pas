763ed49f355c09d816b8ba1570f66f33532a5761
Martin Schlegel <schlegel@itestra.de>
2016-06-30 14:59:06 +0200

    Removed test subfolder

--- test/testRead.pas
+++
@@ -1,7 +0,0 @@
-PROGRAM testRead;
-USES mnh_fileWrappers, mnh_stringUtil;
-VAR accessOk:boolean=false;
-begin
-  if paramCount=1 then writeln(escapeString(fileContent(paramStr(1),accessOk)));
-  writeln('accessOk= ',accessOk);
-end.
