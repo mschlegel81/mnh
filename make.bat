9eb4a48ac137e5c4324ba9728bc0a87b2f6d8191
Martin Schlegel <schlegel@itestra.de>
2016-06-30 10:35:47 +0200

    Fixed building under Linux, simplified under Windows

--- make.bat
+++ make.bat
@@ -1,10 +1,3 @@
-@if not exist core\res_ensureDemos.inc               echo CONST ensureDemos_mnh: array[0..0] of string=(''); > core\res_ensureDemos.inc
-@if not exist core\res_ensureMnhFileAssociations.inc echo CONST ensureMnhFileAssociations_mnh: array[0..0] of string=(''); > core\res_ensureMnhFileAssociations.inc
-@if not exist core\res_ensureNppHighlighting.inc     echo CONST ensureNotepad__Highlighting_mnh: array[0..0] of string=(''); > core\res_ensureNppHighlighting.inc
-@if not exist core\res_ensurePackages.inc            echo CONST ensurePackages_mnh: array[0..0] of string=(''); > core\res_ensurePackages.inc
-@if not exist core\res_examples.inc                  echo CONST doc_examples_txt: array[0..0] of string=(''); > core\res_examples.inc
-@if not exist core\res_html_template.inc             echo CONST doc_html_template_txt: array[0..0] of string=(''); > core\res_html_template.inc
-@if not exist core\res_removeMnhFileAssociations.inc echo CONST removeMnhFileAssociations_mnh: array[0..0] of string=(''); > core\res_removeMnhFileAssociations.inc
 @if not exist core\code_hash.inc                     echo writeln(':xxxxxxxx'); > core\code_hash.inc
 
 @if not exist mnh_light.exe (
--- make.sh
+++ make.sh
@@ -1,17 +1,7 @@
-if [ ! -f core/res_ensureDemos.inc               ]; then echo "CONST ensureDemos_mnh: array[0..0] of string=('');" > core/res_ensureDemos.inc;fi
-if [ ! -f core/res_ensureMnhFileAssociations.inc ]; then echo "CONST ensureMnhFileAssociations_mnh: array[0..0] of string=('');" > core/res_ensureMnhFileAssociations.inc;fi
-if [ ! -f core/res_ensureNppHighlighting.inc     ]; then echo "CONST ensureNotepad__Highlighting_mnh: array[0..0] of string=('');" > core/res_ensureNppHighlighting.inc;fi
-if [ ! -f core/res_ensurePackages.inc            ]; then echo "CONST ensurePackages_mnh: array[0..0] of string=('');" > core/res_ensurePackages.inc;fi
-if [ ! -f core/res_examples.inc                  ]; then echo "CONST doc_examples_txt: array[0..0] of string=('');" > core/res_examples.inc;fi
-if [ ! -f core/res_html_template.inc             ]; then echo "CONST doc_html_template_txt: array[0..0] of string=('');" > core/res_html_template.inc;fi
-if [ ! -f core/res_removeMnhFileAssociations.inc ]; then echo "CONST removeMnhFileAssociations_mnh: array[0..0] of string=('');" > core/res_removeMnhFileAssociations.inc;fi
-if [ ! -f core/code_hash.inc                     ]; then echo "writeln(':xxxxxxxx');" > core/code_hash.inc;fi
+if [ ! -f core/code_hash.inc ]; then echo "writeln(':xxxxxxxx');" > core/code_hash.inc;fi
 lazbuild -B --bm=deployment consoles/mnh_light.lpi
 consoles/mnh_light make.mnh prepare
 lazbuild --bm=deployment consoles/mnh_light.lpi
-mv consoles/mnh_light .
-./mnh_light regTest/regTest.mnh doc/examples.txt
-lazbuild -B --bm=deployment gui/mnh_gui.lpi
-mv gui/mnh .
-./mnh regTest/regTest.mnh doc/examples.txt 
-
+mv consoles/mnh_light ./mnh_tmp
+./mnh_tmp make.mnh test
+rm mnh_tmp
