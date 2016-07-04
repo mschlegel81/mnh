60b16d7d58b0d23b0c5aa3e8e2d1b8276c050902
Martin Schlegel <schlegel@itestra.de>
2016-07-04 14:24:02 +0200

    Enhanced make script for Linux

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 if [ ! -f core/code_hash.inc ]; then echo "writeln(':xxxxxxxx');" > core/code_hash.inc;fi
-lazbuild -B --bm=deployment consoles/mnh_light.lpi
-consoles/mnh_light make.mnh prepare
-lazbuild --bm=deployment consoles/mnh_light.lpi
-mv consoles/mnh_light ./mnh_tmp
+if [ ! -f mnh_light ]; then
+  lazbuild -B --bm=deployment consoles/mnh_light.lpi
+  mv consoles/mnh_light .
+fi
+cp mnh_light mnh_tmp 
 ./mnh_tmp make.mnh test
 rm mnh_tmp
