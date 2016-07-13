add7ac9cfd964229ca07571d8b6a1b85260df73a
Martin Schlegel <schlegel@itestra.de>
2016-07-13 08:50:10 +0200

    Fixed make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @if not exist core\code_hash.inc echo writeln(':xxxxxxxx'); > core\code_hash.inc
 @if not exist mnh_light.exe (
-  ..\lazarus64\lazbuild.exe -B --bm=deployment consoles/mnh_light.lpi
-  move consoles/mnh_light.exe .)  
+  ..\lazarus64\lazbuild.exe -B --bm=deployment consoles\mnh_light.lpi
+  move consoles\mnh_light.exe .)  
 @copy mnh_light.exe mnh_temp.exe
 mnh_temp.exe make.mnh %*
 @del mnh_temp.exe
