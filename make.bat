962629a52c032ece8586002b5f2e7195108bfded
Martin Schlegel <schlegel@itestra.de>
2016-07-13 08:33:47 +0200

    Harmonized Windows/Linux make

--- make.bat
+++ make.bat
@@ -1,8 +1,7 @@
-@if not exist core\code_hash.inc                     echo writeln(':xxxxxxxx'); > core\code_hash.inc
-
+@if not exist core\code_hash.inc echo writeln(':xxxxxxxx'); > core\code_hash.inc
 @if not exist mnh_light.exe (
-  ..\lazarus32\fpc\3.0.0\bin\i386-win32\fpc.exe consoles\mnh_light.lpr -o.\mnh_light.exe -gl -Fu..\lazarus32\components\synedit\units\i386-win32\win32 -Fu..\lazarus32\lcl\units\i386-win32\win32 -Fu..\lazarus32\lcl\units\i386-win32 -Fu..\lazarus32\components\lazutils\lib\i386-win32 -Fu..\lazarus32\packager\units\i386-win32 -Fugui -l -dLCL -dLCLwin32 -MObjFPC -Scgh -Fucore -Fu..\common -Fu..\common\* -Ficore -dversion64bit
-  ..\lazarus32\fpc\3.0.0\bin\i386-win32\delp . core ..\common test consoles bin32 gui\lib\i386-win32 gui\lib\x86_64-win64 gui)
+  ..\lazarus64\lazbuild.exe -B --bm=deployment consoles/mnh_light.lpi
+  move consoles/mnh_light.exe .)  
 @copy mnh_light.exe mnh_temp.exe
 mnh_temp.exe make.mnh %*
 @del mnh_temp.exe
--- make.sh
+++ make.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 if [ ! -f core/code_hash.inc ]; then echo "writeln(':xxxxxxxx');" > core/code_hash.inc;fi
 if [ ! -f mnh_light ]; then
   lazbuild -B --bm=deployment consoles/mnh_light.lpi
   mv consoles/mnh_light .
 fi
 cp mnh_light mnh_tmp 
-./mnh_tmp make.mnh test
+./mnh_tmp make.mnh $@
 rm mnh_tmp
