6401938262166629a3aa8f5a8688dcf817daa0aa
Martin Schlegel <schlegel@itestra.de>
2016-08-05 12:59:57 +0200

    plot/addPlot for expressions

--- core/mnh_plotfuncs.pas
+++ core/mnh_plotfuncs.pas
@@ -2,7 +2,9 @@ UNIT mnh_plotFuncs;
 INTERFACE
 {$WARN 5024 OFF}
 USES sysutils, mnh_funcs,mnh_litVar,mnh_tokLoc,mnh_out_adapters,mnh_plotData,math,mnh_constants,mnh_contexts;
-
+TYPE F_generateRow=FUNCTION(CONST f:P_expressionLiteral; CONST t0,t1:T_myFloat; CONST samples:longint; CONST location:T_tokenLocation; VAR context:T_evaluationContext):T_dataRow;
+FUNCTION newDataRow(CONST y:P_listLiteral; CONST x:P_listLiteral=nil):T_dataRow;
+VAR generateRow:F_generateRow;
 IMPLEMENTATION
 FUNCTION fReal(CONST X: P_literal): double; inline;
   begin
@@ -17,27 +19,41 @@ FUNCTION fReal(CONST X: P_literal): double; inline;
     end;
   end;
 
-FUNCTION addPlot(CONST params: P_listLiteral; CONST tokenLocation:T_tokenLocation; VAR context:T_evaluationContext):P_literal;
-  VAR options: ansistring = '';
-      sizeWithoutOptions: longint;
-      i,imax: longint;
-      X,Y:    P_listLiteral;
-
-      row:    T_dataRow;
-
+FUNCTION newDataRow(CONST y:P_listLiteral; CONST x:P_listLiteral=nil):T_dataRow;
+  VAR i,imax:longint;
+      xy:P_listLiteral;
   PROCEDURE addSample(CONST px,py:double);
     VAR k:longint;
     begin
-      k:=length(row);
-      setLength(row,k+1);
-      row[k,0]:=px;
-      row[k,1]:=py;
+      k:=length(result);
+      setLength(result,k+1);
+      result[k,0]:=px;
+      result[k,1]:=py;
     end;
 
   begin
+    setLength(result,0);
+    if x=nil then case y^.literalType of
+      lt_list:
+        with y^ do for i:=0 to size-1 do if value(i)^.literalType in [lt_intList,lt_realList,lt_numList] then begin
+          xy:=P_listLiteral(value(i));
+          if xy^.size=2 then addSample(fReal(xy^.value(0)), fReal(xy^.value(1)))
+                        else addSample(Nan,Nan);
+        end else addSample(Nan,Nan);
+      lt_intList, lt_realList, lt_numList:
+        with y^ do for i:=0 to size-1 do addSample(i,fReal(value(i)));
+    end else begin
+      imax:=min(X^.size, Y^.size);
+      for i:=0 to imax-1 do addSample(fReal(X^.value(i)), fReal(Y^.value(i)));
+    end;
+  end;
+
+FUNCTION addPlot(CONST params: P_listLiteral; CONST tokenLocation:T_tokenLocation; VAR context:T_evaluationContext):P_literal;
+  VAR options: ansistring = '';
+      sizeWithoutOptions: longint;
+  begin
     result:=nil;
     if (params<>nil) and (params^.size>=1) then begin
-      setLength(row,0);
       if (params^.value(params^.size-1)^.literalType = lt_string) then begin
         options:=P_stringLiteral(params^.value(params^.size-1))^.value;
         sizeWithoutOptions:=params^.size-1;
@@ -45,36 +61,31 @@ FUNCTION addPlot(CONST params: P_listLiteral; CONST tokenLocation:T_tokenLocatio
         options:='';
         sizeWithoutOptions:=params^.size;
       end;
-      if (sizeWithoutOptions = 1) and (params^.value(0)^.literalType = lt_list) then begin
-        with P_listLiteral(params^.value(0))^ do
-        for i:=0 to size-1 do if value(i)^.literalType in [lt_intList,lt_realList,lt_numList] then begin
-          Y:=P_listLiteral(value(i));
-          if Y^.size=2 then addSample(fReal(Y^.value(0)), fReal(Y^.value(1)))
-                       else addSample(Nan,Nan);
-        end else addSample(Nan,Nan);
-        context.adapters^.plot.addRow(options,row);
+      if (sizeWithoutOptions = 1) and (params^.value(0)^.literalType in [lt_list,lt_intList, lt_realList, lt_numList]) then begin
+        context.adapters^.plot.addRow(options,newDataRow(P_listLiteral(params^.value(0))));
         context.adapters^.raiseCustomMessage(mt_plotCreatedWithDeferredDisplay,'',tokenLocation);
-        setLength(row,0);
-        exit(newVoidLiteral);
-      end;
-      if (sizeWithoutOptions = 1) and (params^.value(0)^.literalType in [lt_intList, lt_realList, lt_numList]) then begin
-        with P_listLiteral(params^.value(0))^ do
-        for i:=0 to size-1 do addSample(i,fReal(value(i)));
-        context.adapters^.plot.addRow(options,row);
-        context.adapters^.raiseCustomMessage(mt_plotCreatedWithDeferredDisplay,'',tokenLocation);
-        setLength(row,0);
         exit(newVoidLiteral);
       end;
       if (sizeWithoutOptions = 2) and
          (params^.value(0)^.literalType in [lt_intList, lt_realList, lt_numList]) and
-         (params^.value(1)^.literalType in [lt_intList, lt_realList, lt_numList]) then begin
-        X:=P_listLiteral(params^.value(0));
-        Y:=P_listLiteral(params^.value(1));
-        imax:=min(X^.size, Y^.size);
-        for i:=0 to imax-1 do addSample(fReal(X^.value(i)), fReal(Y^.value(i)));
-        context.adapters^.plot.addRow(options,row);
+         (params^.value(1)^.literalType in [lt_intList, lt_realList, lt_numList]) and
+         (P_listLiteral(params^.value(0))^.size=P_listLiteral(params^.value(1))^.size) then begin
+        context.adapters^.plot.addRow(options,newDataRow(P_listLiteral(params^.value(0)),P_listLiteral(params^.value(1))));
         context.adapters^.raiseCustomMessage(mt_plotCreatedWithDeferredDisplay,'',tokenLocation);
-        setLength(row,0);
+        exit(newVoidLiteral);
+      end;
+      if (sizeWithoutOptions = 4) and
+         (params^.value(0)^.literalType=lt_expression) and (P_expressionLiteral(params^.value(0))^.arity=1) and
+         (params^.value(1)^.literalType in [lt_int,lt_real]) and
+         (params^.value(2)^.literalType in [lt_int,lt_real]) and (params^.value(2)^.isInRelationTo(tt_comparatorGrt,params^.value(1))) and
+         (params^.value(3)^.literalType=lt_int) and (P_intLiteral(params^.value(3))^.value>=2) then begin
+        context.adapters^.plot.addRow(options,generateRow(P_expressionLiteral(params^.value(0)),
+                                                          fReal(params^.value(1)),
+                                                          fReal(params^.value(2)),
+                                                          P_intLiteral(params^.value(3))^.value,
+                                                          tokenLocation,
+                                                          context));
+        if context.adapters^.noErrors then context.adapters^.raiseCustomMessage(mt_plotCreatedWithDeferredDisplay,'',tokenLocation);
         exit(newVoidLiteral);
       end;
     end;
@@ -83,7 +94,6 @@ FUNCTION addPlot(CONST params: P_listLiteral; CONST tokenLocation:T_tokenLocatio
 FUNCTION plot(CONST params: P_listLiteral; CONST tokenLocation:T_tokenLocation; VAR context:T_evaluationContext):P_literal;
   begin
     context.adapters^.plot.clear;
-    context.adapters^.raiseCustomMessage(mt_plotCreatedWithDeferredDisplay,'',tokenLocation);
     if (params=nil) or (params^.size=0) or (params^.size = 1) and (params^.value(0)^.literalType = lt_emptyList)
     then result:=newVoidLiteral
     else result:=addPlot(params, tokenLocation,context);
@@ -267,8 +277,7 @@ INITIALIZATION
   mnh_funcs.registerRule(PLOT_NAMESPACE,'plot', @plot,
     'plot(list,[options]); //plots flat numeric list or xy-list'+
     '#plot(xList,yList,[options]); //plots flat numeric list or xy-list'+
-    '#plot(yExpression,t0,t1,samples,[options]); //plots yExpression versus t in [t0,t1]'+
-    '#plot(xExpression,yExpression,t0,t1,samples,[options]); //plots yExpression versus xExpression for t in [t0,t1]'+
+    '#plot(f:expression(1),t0,t1>t0,samples>=2,[options]); //plots f versus t in [t0,t1]'+
     '#options are optional and given in the form of a string, the individual option items being delimited by spaces'+
     '#valid options are:'+'#Style/size modifier: any real number'+
     '#Styles:'+'#  line; l;'+'#  stepLeft;'+'#  stepRight;'+
@@ -282,7 +291,7 @@ INITIALIZATION
   mnh_funcs.registerRule(PLOT_NAMESPACE,'addPlot', @addPlot,
     'addPlot(list,[options]); //adds plot of flat numeric list or xy-list'+
     '#addPlot(xList,yList,[options]); //adds plot of flat numeric list or xy-list'+
-    '#addPlot(yExpression,t0,t1,samples,[options]); //adds plot of yExpression versus t in [t0,t1]'+'#addPlot(xExpression,yExpression,t0,t1,samples,[options]); //adds plot of yExpression versus xExpression for t in [t0,t1]');
+    '#addPlot(f:expression(1),t0,t1>t0,samples>=2,[options]); //adds plot of f versus t in [t0,t1]');
   mnh_funcs.registerRule(PLOT_NAMESPACE,'getOptions',@getOptions,
     'getOptions;#returns plot options as a key-value-list.');
   mnh_funcs.registerRule(PLOT_NAMESPACE,'setOptions',@setOptions,
