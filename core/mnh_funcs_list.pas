936a15a1eb35dcf4ef959514bbf6dcec734ff503
Martin Schlegel <schlegel@itestra.de>
2017-01-18 13:21:24 +0100

    Fixed head, tail, leading, trailing, sort and unique. Reintroduced trueCount. Added getAll

--- core/mnh_funcs_list.pas
+++ core/mnh_funcs_list.pas
@@ -1,7 +1,7 @@
 UNIT mnh_funcs_list;
 INTERFACE
 {$WARN 5024 OFF}
-USES mnh_basicTypes,mnh_litVar,mnh_constants, mnh_funcs,mnh_out_adapters,mnh_contexts,myGenerics;
+USES mnh_basicTypes,mnh_litVar,mnh_constants, mnh_funcs,mnh_out_adapters,mnh_contexts;
 VAR BUILTIN_HEAD,BUILTIN_GET,BUILTIN_TAIL:P_intFuncCallback;
 
 IMPLEMENTATION
@@ -11,10 +11,10 @@ VAR builtinLocation_sort:T_identifiedInternalFunction;
 begin
   result:=nil;
   if (params<>nil) and (params^.size>=1) then begin
-    if (arg0^.literalType in C_listTypes) then begin
+    if arg0^.literalType in C_listTypes then begin
       if      (params^.size=1) then result:=list0^.CALL_MACRO
       else if (params^.size=2) and (arg1^.literalType=lt_int) then result:=list0^.CALL_MACRO(int1^.value);
-    end else SCALAR_FALLBACK;
+    end else if arg0^.literalType in C_scalarTypes then SCALAR_FALLBACK;
   end;
 end}
 
@@ -45,20 +45,31 @@ SUB_LIST_IMPL;
 FUNCTION sort_imp intFuncSignature;
   begin
     result:=nil;
-    if (params<>nil) and (params^.size=1) and (arg0^.literalType in C_listTypes) then begin
-      cloneOrCopyList0;
-      P_listLiteral(result)^.sort;
+    if (params<>nil) and (params^.size=1) and (arg0^.literalType in C_compoundTypes) then begin
+      if arg0^.literalType in C_listTypes then begin
+        cloneOrCopyList0;
+        P_listLiteral(result)^.sort;
+      end else
+      if arg0^.literalType in C_setTypes
+      then result:=set0^.getManifestation^.rereferenced
+      else result:=map0^.getManifestation^.rereferenced;
     end else if (params<>nil) and (params^.size=2)
-            and (arg0^.literalType in C_listTypes)
+            and (arg0^.literalType in C_compoundTypes)
             and (arg1^.literalType=lt_expression) then begin
-      cloneOrCopyList0;
+      if arg0^.literalType in C_listTypes then cloneOrCopyList0
+      else if arg0^.literalType in C_setTypes
+      then result:=set0^.getManifestation^.clone
+      else result:=map0^.getManifestation^.clone;
       context.callStackPush(tokenLocation,@builtinLocation_sort,params,nil);
       P_listLiteral(result)^.customSort(P_expressionLiteral(arg1),tokenLocation,context.adapters^);
       context.callStackPop();
     end else if (params<>nil) and (params^.size=2)
-            and (arg0^.literalType in C_listTypes)
+            and (arg0^.literalType in C_compoundTypes)
             and (arg1^.literalType=lt_int) then begin
-      cloneOrCopyList0;
+      if arg0^.literalType in C_listTypes then cloneOrCopyList0
+      else if arg0^.literalType in C_setTypes
+      then result:=set0^.getManifestation^.clone
+      else result:=map0^.getManifestation^.clone;
       P_listLiteral(result)^.sortBySubIndex(int1^.value,tokenLocation,context.adapters^);
     end
   end;
@@ -73,9 +84,11 @@ FUNCTION sortPerm_imp intFuncSignature;
 FUNCTION unique_imp intFuncSignature;
   begin
     result:=nil;
-    if (params<>nil) and (params^.size=1) and (arg0^.literalType in C_listTypes) then begin
-      cloneOrCopyList0;
-      P_listLiteral(result)^.unique;
+    if (params<>nil) and (params^.size=1) then begin
+      if (arg0^.literalType in C_listTypes) then begin
+        cloneOrCopyList0;
+        P_listLiteral(result)^.unique;
+      end else if (arg0^.literalType in C_setTypes) then exit(arg0^.rereferenced);
     end;
   end;
 
@@ -133,68 +146,6 @@ FUNCTION flatten_imp intFuncSignature;
     end else result:=nil;
   end;
 
-//FUNCTION unflatten_imp intFuncSignature;
-//  VAR openerCloser:array of array[0..1] of P_literal;
-//      i:longint;
-//      iterIdx:longint=0;
-//      failed:boolean=false;
-//
-//  FUNCTION unflatten(CONST stopAt:P_literal):P_listLiteral;
-//    FUNCTION matchingCloser(CONST p:P_literal):P_literal;
-//      VAR k:longint;
-//      begin
-//        for k:=0 to length(openerCloser)-1 do
-//        if openerCloser[k,0]^.equals(p) then exit(openerCloser[k,1]);
-//        result:=nil;
-//      end;
-//
-//    VAR nextCloser,nextLiteral:P_literal;
-//    begin
-//      result:=newListLiteral();
-//      while (iterIdx<list0^.size) do begin
-//        nextLiteral:=list0^.value[iterIdx];
-//        inc(iterIdx);
-//        if (stopAt<>nil) and (stopAt^.equals(nextLiteral)) then exit(result);
-//        nextCloser:=matchingCloser(nextLiteral);
-//        if nextCloser=nil
-//        then result^.append(nextLiteral,true)
-//        else result^.append(unflatten(nextCloser),false);
-//      end;
-//      if stopAt<>nil then failed:=true;
-//    end;
-//
-//  begin
-//    result:=nil;
-//    if (params<>nil) and (params^.size=3) and
-//       (arg0^.literalType in C_matchingTypes[tt_typeCheckFlatList]) and
-//       ((arg1^.literalType in C_matchingTypes[tt_typeCheckFlatList]) and (list1^.size>0) and
-//        (arg2^.literalType in C_matchingTypes[tt_typeCheckFlatList]) and (list2^.size=list1^.size)
-//      or (arg1^.literalType in C_matchingTypes[tt_typeCheckScalar]) and
-//         (arg2^.literalType in C_matchingTypes[tt_typeCheckScalar])) then begin
-//      if (list0^.size<=0) then begin
-//        arg0^.rereference;
-//        exit(arg0);
-//      end;
-//      if (arg1^.literalType in C_matchingTypes[tt_typeCheckFlatList]) then begin
-//        setLength(openerCloser,list1^.size);
-//        for i:=0 to list1^.size-1 do begin
-//          openerCloser[i,0]:=list1^.value(i);
-//          openerCloser[i,1]:=list2^.value(i);
-//        end;
-//      end else begin
-//        setLength(openerCloser,1);
-//        openerCloser[0,0]:=arg1;
-//        openerCloser[0,1]:=arg2;
-//      end;
-//      result:=unflatten(nil);
-//      if failed then begin
-//        disposeLiteral(result);
-//        context.adapters^.raiseError('Error in unflatten: unbalanced paranthesis found',tokenLocation);
-//      end;
-//      setLength(openerCloser,0);
-//    end;
-//  end;
-
 FUNCTION size_imp intFuncSignature;
   begin
     result:=nil;
@@ -205,23 +156,23 @@ FUNCTION size_imp intFuncSignature;
     end;
   end;
 
-//FUNCTION trueCount_impl intFuncSignature;
-//  VAR i,c:longint;
-//  begin
-//    result:=nil;
-//    if (params<>nil) and (params^.size=1) then begin
-//      case arg0^.literalType of
-//        lt_boolean: if bool0^.value then exit(newIntLiteral(1)) else exit(newIntLiteral(0));
-//        lt_booleanList,lt_booleanSet: begin
-//          c:=0;
-//          for i:=0 to compound0^.size-1 do if P_boolLiteral(compound0^.value[i])^.value then inc(c);
-//          exit(newIntLiteral(c));
-//        end;
-//        lt_emptyList,lt_emptySet: exit(newIntLiteral(0));
-//      end;
-//    end;
-//  end;
-//
+FUNCTION trueCount_impl intFuncSignature;
+  VAR i,c:longint;
+  begin
+    result:=nil;
+    if (params<>nil) and (params^.size=1) then begin
+      case arg0^.literalType of
+        lt_boolean: if bool0^.value then exit(newIntLiteral(1)) else exit(newIntLiteral(0));
+        lt_booleanList,lt_booleanSet: begin
+          c:=0;
+          for i:=0 to compound0^.size-1 do if P_boolLiteral(compound0^[i])^.value then inc(c);
+          exit(newIntLiteral(c));
+        end;
+        lt_emptyList,lt_emptySet: exit(newIntLiteral(0));
+      end;
+    end;
+  end;
+
 FUNCTION reverseList_impl intFuncSignature;
   VAR i:longint;
   begin
@@ -248,16 +199,37 @@ FUNCTION get_imp intFuncSignature;
             .append(arg1,true);
       result:=get_imp(@tmpPar,tokenLocation,context);
       tmpPar.destroy;
-      if result^.literalType=lt_void then exit(result);
-      if result<>nil then begin
-        tmpPar.create(params^.size-1);
-        tmpPar.append(result,false);
-        for i:=2 to params^.size-1 do tmpPar.append(params^[i],true);
-        result:=get_imp(@tmpPar,tokenLocation,context);
-        tmpPar.destroy;
-      end;
+      if (result=nil) or (result^.literalType=lt_void) then exit(result);
+      tmpPar.create(params^.size-1);
+      tmpPar.append(result,false);
+      for i:=2 to params^.size-1 do tmpPar.append(params^[i],true);
+      result:=get_imp(@tmpPar,tokenLocation,context);
+      tmpPar.destroy;
     end
     else result:=nil;
+  end;
+
+FUNCTION getAll_imp intFuncSignature;
+  VAR iter:T_arrayOfLiteral;
+      sub:P_literal;
+      got:P_literal;
+  begin
+    result:=nil;
+    if (params<>nil) and (params^.size=2) and (arg0^.literalType in C_compoundTypes) and (arg1^.literalType in C_compoundTypes) then begin
+      if arg1^.literalType in C_listTypes
+      then result:=newListLiteral
+      else result:=newSetLiteral;
+      iter:=compound1^.iteratableList;
+      for sub in iter do if result<>nil then begin
+        got:=compound0^.get(sub);
+        if got<>nil then begin
+          if got^.literalType<>lt_void
+          then collResult^.append(got,false)
+          else disposeLiteral(got);
+        end else disposeLiteral(result);
+      end;
+      disposeLiteral(iter);
+    end;
   end;
 
 FUNCTION getInner_imp intFuncSignature;
@@ -295,7 +267,6 @@ FUNCTION indexOf_impl intFuncSignature;
 
 INITIALIZATION
   //Functions on lists:
-//  registerRule(LIST_NAMESPACE,'add'     ,@add_imp     ,true,ak_binary    ,'add(L,e);//Returns L with the new element e appended');
   BUILTIN_HEAD:=
   registerRule(LIST_NAMESPACE,'head'    ,@head_imp    ,true,ak_variadic_1,'head(L);//Returns the first element of list L or [] if L is empty#head(L,k);//Returns the first min(k,size(L)) elements of L or [] if L is empty');
   BUILTIN_TAIL:=
@@ -309,22 +280,19 @@ INITIALIZATION
                                                'sort(L,innerIndex:int);//Returns L sorted by given inner index');
   registerRule(LIST_NAMESPACE,'sortPerm'        ,@sortPerm_imp      ,true,ak_unary     ,'sortPerm(L);//Returns indexes I so that L%I==sort(L)');
   registerRule(LIST_NAMESPACE,'unique'          ,@unique_imp        ,true,ak_unary     ,'unique(L:list);//Returns list L without duplicates and enhanced for faster lookup');
-//registerRule(LIST_NAMESPACE,'toMap'           ,@mapOf_imp         ,true,ak_unary     ,'toMap(L:keyValueList);//Returns key L without duplicate keys and enhanced for faster lookup');
   registerRule(LIST_NAMESPACE,'elementFrequency',@getElementFreqency,true,ak_unary     ,'elementFrequency(L);//Returns a list of pairs [count,e] containing distinct elements e of L and their respective frequencies');
-  registerRule(LIST_NAMESPACE,'transpose'       ,@transpose_imp     ,true,ak_unary     ,'transpose(L,filler);//Returns list L transposed. If lists have different lengths, filler is used.');
+  registerRule(LIST_NAMESPACE,'transpose'       ,@transpose_imp     ,true,ak_unary     ,'transpose(L,filler);//Returns list L transposed. If sub lists of L have different lengths, filler is used.');
   registerRule(LIST_NAMESPACE,'union'           ,@setUnion_imp      ,true,ak_variadic_1,'union(A,...);//Returns a union of all given parameters. All parameters must be lists.');
   registerRule(LIST_NAMESPACE,'intersect'       ,@setIntersect_imp  ,true,ak_variadic_1,'intersect(A,...);//Returns an intersection of all given parameters. All parameters must be lists.');
   registerRule(LIST_NAMESPACE,'minus'           ,@setMinus_imp      ,true,ak_binary    ,'minus(A,B);//Returns the asymmetric set difference of A and B. All parameters must be lists.');
   registerRule(LIST_NAMESPACE,'flatten'         ,@flatten_imp       ,true,ak_variadic  ,'flatten(L,...);//Returns all parameters as a flat list.');
-//registerRule(LIST_NAMESPACE,'unflatten'       ,@unflatten_imp     ,true,ak_ternary   ,'unflatten(L,openers,closers);//Returns L expanded to a nexted list using openers and closers.');
   registerRule(LIST_NAMESPACE,'size'            ,@size_imp          ,true,ak_unary     ,'size(L);//Returns the number of elements in list L');
-//registerRule(LIST_NAMESPACE,'trueCount'       ,@trueCount_impl    ,true,ak_unary     ,'trueCount(B:booleanList);//Returns the number of true values in B');
+  registerRule(LIST_NAMESPACE,'trueCount'       ,@trueCount_impl    ,true,ak_unary     ,'trueCount(B:booleanList);//Returns the number of true values in B');
   registerRule(LIST_NAMESPACE,'reverseList'     ,@reverseList_impl  ,true,ak_unary     ,'reverseList(L:list);//Returns L reversed');
   BUILTIN_GET:=
-  registerRule(LIST_NAMESPACE,'get'     ,@get_imp     ,true,ak_variadic_2,'get(L:list,index);');
+  registerRule(LIST_NAMESPACE,'get'     ,@get_imp     ,true,ak_variadic_2,'get(L,accessor);//Returns elements of list, set or map L by accessor');
+  registerRule(LIST_NAMESPACE,'getAll'  ,@getAll_imp  ,true,ak_binary    ,'getAll(L,accessors);//Returns elements of list, set or map L by collection of accessors');
   registerRule(LIST_NAMESPACE,'getInner',@getInner_imp,true,ak_variadic_2,'getInner(L:list,index);');
-//registerRule(LIST_NAMESPACE,'mapGet'  ,@mapGet_imp  ,true,ak_variadic_2,'mapGet(L:keyValueList,key:string);//Returns the element with matching key or the empty list if no such element was found.#'+
-//                                                                        'mapGet(L:keyValueList,key:string,fallback);//Returns the element with matching key or fallback if no such element was found.');
   registerRule(LIST_NAMESPACE,'indexOf' ,@indexOf_impl,true,ak_unary     ,'indexOf(B:booleanList);//Returns the indexes for which B is true.');
 
 FINALIZATION
