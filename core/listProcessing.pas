861c7445c8d1ab77a00a5d743e45f016267ae24a
Martin Schlegel <schlegel@itestra.de>
2017-04-06 17:12:01 +0200

    Builtin function pMap

--- core/listProcessing.pas
+++ core/listProcessing.pas
@@ -11,6 +11,10 @@ PROCEDURE processListSerial(CONST inputList:T_arrayOfLiteral; CONST rulesList:T_
 PROCEDURE processListParallel(CONST inputList:T_arrayOfLiteral; CONST rulesList:T_expressionList; CONST aggregator:P_aggregator;
                               CONST eachLocation:T_tokenLocation;
                               VAR context:T_threadContext);
+FUNCTION processMapParallel(CONST inputList:T_arrayOfLiteral; CONST expr:P_expressionLiteral;
+                            CONST mapLocation:T_tokenLocation;
+                            VAR context:T_threadContext):P_listLiteral;
+
 IMPLEMENTATION
 PROCEDURE processListSerial(CONST inputList: T_arrayOfLiteral;
   CONST rulesList: T_expressionList; CONST aggregator: P_aggregator;
@@ -105,6 +109,83 @@ PROCEDURE processListParallel(CONST inputList: T_arrayOfLiteral;
       dec(fill);
       dispose(dat[fill],destroy);
     end;
+  end;
+
+FUNCTION processMapParallel(CONST inputList:T_arrayOfLiteral; CONST expr:P_expressionLiteral;
+                            CONST mapLocation:T_tokenLocation;
+                            VAR context:T_threadContext):P_listLiteral;
+
+  VAR firstToAggregate:P_futureTask=nil;
+      lastToAggregate:P_futureTask=nil;
+      resultLiteral:P_listLiteral;
+
+  PROCEDURE enqueueForAggregation(CONST task:P_futureTask); inline;
+    begin
+      if firstToAggregate=nil then begin
+        firstToAggregate:=task;
+        lastToAggregate:=task;
+      end else begin
+        lastToAggregate^.nextToAggregate:=task;
+        lastToAggregate:=task;
+      end;
+    end;
+  VAR recycling:record
+        dat:array[0..31] of P_futureTask;
+        fill:longint;
+      end;
+
+  FUNCTION canAggregate:boolean; inline;
+    VAR toAggregate:P_futureTask;
+    begin
+      result:=false;
+      while (firstToAggregate<>nil) and (firstToAggregate^.canGetResult) do begin
+        result:=true;
+        toAggregate:=firstToAggregate;
+        firstToAggregate:=firstToAggregate^.nextToAggregate;
+        resultLiteral^.append(toAggregate^.getResultAsLiteral,false);
+        with recycling do if fill<length(dat) then begin
+          dat[fill]:=toAggregate;
+          inc(fill);
+        end else dispose(toAggregate,destroy);
+      end;
+    end;
+
+  VAR values:P_valueStore;
+      taskQueue:P_taskQueue;
+
+  FUNCTION createTask(CONST x:P_literal):P_futureTask; inline;
+    begin
+      with recycling do if fill>0 then begin
+        dec(fill);
+        result:=dat[fill];
+      end else new(result,create);
+      result^.define(expr,expr^.getLocation,-1,x,@context,values);
+      taskQueue^.enqueue(result,@context);
+    end;
+
+  VAR x:P_literal;
+      aimEnqueueCount:longint;
+  begin
+    resultLiteral:=newListLiteral(length(inputList));
+    recycling.fill:=0;
+    taskQueue:=context.getParent^.getTaskQueue;
+    values:=context.valueStore.readOnlyClone;
+    aimEnqueueCount:=workerThreadCount*2+1;
+    for x in inputList do if context.adapters^.noErrors then begin
+      enqueueForAggregation(createTask(x));
+      if taskQueue^.getQueuedCount>aimEnqueueCount then begin
+        if not(canAggregate) then taskQueue^.activeDeqeue(context);
+        //if there is not enough pending after dequeuing, increase aimEnqueueCount
+        if taskQueue^.getQueuedCount<workerThreadCount then inc(aimEnqueueCount,workerThreadCount);
+      end;
+    end;
+    while firstToAggregate<>nil do if not(canAggregate) then taskQueue^.activeDeqeue(context);
+    dispose(values,destroy);
+    with recycling do while fill>0 do begin
+      dec(fill);
+      dispose(dat[fill],destroy);
+    end;
+    result:=resultLiteral;
   end;
 
 end.
