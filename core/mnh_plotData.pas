d1f13c0dab5c09f5bad3ff9fc65638d28bb39037
Martin Schlegel <schlegel@itestra.de>
2017-07-14 09:51:38 +0200

    Plot: grid tics are temporary variabled during rendering only

--- core/mnh_plotData.pas
+++ core/mnh_plotData.pas
@@ -11,22 +11,15 @@ TYPE
   T_boundingBox = array['x'..'y', 0..1] of double;
   P_plot =^T_plot;
   T_plot = object
-    TYPE T_ticInfo = record
-      pos: double;
-      major: boolean;
-      txt: ansistring;
-    end;
     private
       cs: TRTLCriticalSection;
       scalingOptions:T_scalingOptions;
-
-      tic: T_ticInfos;
       row: array of T_sampleRow;
 
       PROCEDURE setScalingOptions(CONST value:T_scalingOptions);
       FUNCTION  getScalingOptions:T_scalingOptions;
-      PROCEDURE drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: longint);
-      PROCEDURE drawCoordSys(CONST target: TCanvas; CONST intendedWidth,intendedHeight:longint);
+      PROCEDURE drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: longint; VAR gridTic: T_ticInfos);
+      PROCEDURE drawCoordSys(CONST target: TCanvas; CONST intendedWidth,intendedHeight:longint; VAR gridTic: T_ticInfos);
       PROCEDURE drawCustomText(CONST target: TCanvas; CONST intendedWidth,intendedHeight:longint);
       FUNCTION  obtainPlot(CONST width,height,supersampling:longint):TImage;
     public
@@ -174,7 +167,7 @@ PROCEDURE T_plot.panByPixels(CONST pixelDX, pixelDY: longint; VAR plotImage: TIm
     system.leaveCriticalSection(cs);
   end; end;
 
-PROCEDURE T_plot.drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: longint);
+PROCEDURE T_plot.drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: longint; VAR gridTic: T_ticInfos);
   VAR rowId, i, yBaseLine:longint;
       lastX: longint = 0;
       lastY: longint = 0;
@@ -217,12 +210,12 @@ PROCEDURE T_plot.drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: lon
     target.Pen.color:=scaleAndColor.lineColor;
     target.Pen.width:=scaleAndColor.lineWidth;
     if (gse_fineGrid in scalingOptions.axisStyle['y']) then
-    for i:=0 to length(tic['y'])-1 do with tic['y'][i] do if not(major) then begin
+    for i:=0 to length(gridTic['y'])-1 do with gridTic['y'][i] do if not(major) then begin
       lastY:=round(pos*scalingFactor);
       target.line(0, lastY, target.width, lastY);
     end;
     if (gse_fineGrid in scalingOptions.axisStyle['x']) then
-    for i:=0 to length(tic['x'])-1 do with tic['x'][i] do if not(major) then begin
+    for i:=0 to length(gridTic['x'])-1 do with gridTic['x'][i] do if not(major) then begin
       lastX:=round(pos*scalingFactor);
       target.line(lastX, 0, lastX, target.height);
     end;
@@ -232,12 +225,12 @@ PROCEDURE T_plot.drawGridAndRows(CONST target: TCanvas; CONST scalingFactor: lon
     target.Pen.color:=scaleAndColor.lineColor;
     target.Pen.width:=scaleAndColor.lineWidth;
     if (gse_coarseGrid in scalingOptions.axisStyle['y']) then
-    for i:=0 to length(tic['y'])-1 do with tic['y'][i] do if major then begin
+    for i:=0 to length(gridTic['y'])-1 do with gridTic['y'][i] do if major then begin
       lastY:=round(pos*scalingFactor);
       target.line(0, lastY, target.width, lastY);
     end;
     if (gse_coarseGrid in scalingOptions.axisStyle['x']) then
-    for i:=0 to length(tic['x'])-1 do with tic['x'][i] do if major then begin
+    for i:=0 to length(gridTic['x'])-1 do with gridTic['x'][i] do if major then begin
       lastX:=round(pos*scalingFactor);
       target.line(lastX, 0, lastX, target.height);
     end;
@@ -443,7 +436,7 @@ PROCEDURE T_plot.drawCustomText(CONST target: TCanvas; CONST intendedWidth,inten
     end;
   end;
 
-PROCEDURE T_plot.drawCoordSys(CONST target: TCanvas; CONST intendedWidth,intendedHeight:longint);
+PROCEDURE T_plot.drawCoordSys(CONST target: TCanvas; CONST intendedWidth,intendedHeight:longint; VAR gridTic: T_ticInfos);
   VAR i, x, y: longint;
       cSysX,cSysY:longint;
   begin
@@ -469,12 +462,12 @@ PROCEDURE T_plot.drawCoordSys(CONST target: TCanvas; CONST intendedWidth,intende
     if (scalingOptions.axisStyle['x']<>[]) then target.line(target.width, cSysY, cSysX, cSysY);
     //-------------------------------------------------------------------:axis
     //tics:-------------------------------------------------------------------
-    if (scalingOptions.axisStyle['y']<>[]) then for i:=0 to length(tic['y'])-1 do with tic['y'][i] do if major then begin
+    if (scalingOptions.axisStyle['y']<>[]) then for i:=0 to length(gridTic['y'])-1 do with gridTic['y'][i] do if major then begin
       y:=round(pos);
       target.line(cSysX-5, y, cSysX, y);
       if (gse_tics in scalingOptions.axisStyle['y']) then target.textOut(cSysX-5-target.TextWidth(txt), y-target.TextHeight(txt) shr 1, txt);
     end;
-    if (scalingOptions.axisStyle['x']<>[]) then for i:=0 to length(tic['x'])-1 do with tic['x'][i] do if major then begin
+    if (scalingOptions.axisStyle['x']<>[]) then for i:=0 to length(gridTic['x'])-1 do with gridTic['x'][i] do if major then begin
       x:=round(pos);
       target.line(x, cSysY+5, x, cSysY);
       if (gse_tics in scalingOptions.axisStyle['x']) then target.textOut(x-target.TextWidth(txt) shr 1, cSysY+5, txt);
@@ -496,17 +489,18 @@ PROCEDURE scale(source: TImage; VAR dest: TImage; CONST factor: double);
 
 PROCEDURE T_plot.renderPlot(VAR plotImage: TImage; CONST supersampling:longint);
   VAR renderImage:TImage;
+      gridTics:T_ticInfos;
   begin
     system.enterCriticalSection(cs);
     try
-      scalingOptions.updateForPlot(plotImage.Canvas,plotImage.width,plotImage.height,row,tic);
+      scalingOptions.updateForPlot(plotImage.Canvas,plotImage.width,plotImage.height,row,gridTics);
       renderImage:=TImage.create(nil);
       renderImage.SetInitialBounds(0,0,plotImage.width*supersampling,plotImage.height*supersampling);
-      drawGridAndRows(renderImage.Canvas,supersampling);
+      drawGridAndRows(renderImage.Canvas,supersampling,gridTics);
       scale(renderImage,plotImage,1/supersampling);
       renderImage.destroy;
       drawCustomText(plotImage.Canvas,plotImage.width,plotImage.height);
-      drawCoordSys(plotImage.Canvas,plotImage.width,plotImage.height);
+      drawCoordSys(plotImage.Canvas,plotImage.width,plotImage.height,gridTics);
     finally
       system.leaveCriticalSection(cs);
     end;
@@ -514,19 +508,20 @@ PROCEDURE T_plot.renderPlot(VAR plotImage: TImage; CONST supersampling:longint);
 
 FUNCTION T_plot.obtainPlot(CONST width,height,supersampling:longint):TImage;
   VAR renderImage:TImage;
+      gridTics:T_ticInfos;
   begin
     system.enterCriticalSection(cs);
     try
       renderImage:=TImage.create(nil);
       renderImage.SetInitialBounds(0,0,width*supersampling,height*supersampling);
-      scalingOptions.updateForPlot(renderImage.Canvas,width,height,row,tic);
-      drawGridAndRows(renderImage.Canvas,supersampling);
+      scalingOptions.updateForPlot(renderImage.Canvas,width,height,row,gridTics);
+      drawGridAndRows(renderImage.Canvas,supersampling,gridTics);
       result:=TImage.create(nil);
       result.SetInitialBounds(0,0,width,height);
       scale(renderImage,result,1/supersampling);
       renderImage.destroy;
       drawCustomText(result.Canvas,width,height);
-      drawCoordSys(result.Canvas,width,height);
+      drawCoordSys(result.Canvas,width,height,gridTics);
     finally
       system.leaveCriticalSection(cs);
     end;
@@ -554,16 +549,10 @@ FUNCTION T_plot.renderToString(CONST width, height, supersampling: longint): ans
   end;
 
 PROCEDURE T_plot.CopyFrom(VAR p:T_plot);
-  VAR axis:char;
-      i:longint;
+  VAR i:longint;
   begin
     system.enterCriticalSection(cs);
     system.enterCriticalSection(p.cs);
-
-    for axis:='x' to 'y' do begin
-      setLength(tic[axis],length(p.tic[axis]));
-      for i:=0 to length(tic[axis])-1 do tic[axis,i]:=p.tic[axis,i];
-    end;
     scalingOptions:=p.scalingOptions;
     for i:=0 to length(row)-1 do row[i].destroy;
     setLength(row,length(p.row));
