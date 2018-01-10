7ebdaed4e26dcf166dc4f1f869e19921242567f9
Martin Schlegel <m.schlegel81@googlemail.com>
2018-01-10 10:13:47 +0100

    Fixed make

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f core/code_hash.inc ]; then echo "CODE_HASH='xxxxxxxx';" > core/code_hash.inc;fi
-if [ ! -f core/build_number.inc ]; then echo "BUILD_NUMBER=0;" > core/built_number.inc;fi
+if [ ! -f core/build_number.inc ]; then echo "BUILD_NUMBER=0;" > core/build_number.inc;fi
 if [ ! -f mnh_light ]; then
   lazbuild -B --bm=deployment consoles/mnh_light.lpi
   mv consoles/mnh_light .
