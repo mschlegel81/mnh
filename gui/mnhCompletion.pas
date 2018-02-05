84c8f580c45ee963fd822ad0cd865e26d8f06df2
Martin Schlegel <m.schlegel81@googlemail.com>
2018-02-05 09:46:22 +0100

    Fixed completion

--- gui/mnhCompletion.pas
+++ gui/mnhCompletion.pas
@@ -79,7 +79,6 @@ PROCEDURE T_completionLogic.ensureWordsInEditorForCompletion;
 
   begin
     caret:=editor.CaretXY;
-    if (wordsInEditor.size>0) and (lastWordsCaret=caret.y) then exit;
     isUseClause:=(pos(C_tokenInfo[tt_use    ].defaultId,editor.lines[caret.y-1])>0)
               or (pos(C_tokenInfo[tt_include].defaultId,editor.lines[caret.y-1])>0);
     lastWordsCaret:=caret.y;
