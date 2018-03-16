fe9c9bbde9de322dcc391fdeb13cdde268893d75
Martin Schlegel <m.schlegel81@googlemail.com>
2018-03-16 11:35:57 +0100

    Build 185 (with fixed STACK_DEPTH_LIMIT)

--- core/mnh_constants.pas
+++ core/mnh_constants.pas
@@ -2,7 +2,7 @@ UNIT mnh_constants;
 INTERFACE
 USES sysutils;
 CONST
-  STACK_DEPTH_LIMIT={$ifdef Windows}63000{$else}{$ifdef debugMode}2000{$else}4100{$endif}{$endif};
+  STACK_DEPTH_LIMIT={$ifdef Windows}43000{$else}{$ifdef debugMode}2000{$else}4100{$endif}{$endif};
   {$i code_hash.inc}
   {$i build_number.inc}
   FLAVOUR_STRING={$ifdef fullVersion}'F'{$else}'L'{$endif}+
