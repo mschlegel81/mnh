1e9669bbfc42a64214fd5ba430ed91c217c1bfab
Martin Schlegel <m.schlegel81@googlemail.com>
2022-09-02 14:14:15 +0200

    Fixes after merge

--- ide/imig/mnh_imig_form.pas
+++ ide/imig/mnh_imig_form.pas
@@ -200,9 +200,9 @@ PROCEDURE TDisplayImageForm.displayCurrentImage;
 FUNCTION getScreenSize_imp intFuncSignature;
   begin
     result:=nil;
-    if (params=nil) or (params^.size=0) then result:=literalRecycler.newListLiteral(
-      literalRecycler.newIntLiteral(screen.width),
-      literalRecycler.newIntLiteral(screen.height));
+    if (params=nil) or (params^.size=0) then result:=recycler^.listLiteralOf(
+      recycler^.newIntLiteral(screen.width),
+      recycler^.newIntLiteral(screen.height));
   end;
 
 FUNCTION imigImageSize_imp intFuncSignature;
@@ -215,7 +215,7 @@ FUNCTION imigImageSize_imp intFuncSignature;
     new(closedRequest,createRetrieveRequest);
     context^.messages^.postCustomMessage(closedRequest);
     closedRequest^.getResponseWaiting(context^.messages,plotWidth,plotHeight);
-    result:=literalRecycler.newListLiteral()^.appendInt(plotWidth)^.appendInt(plotHeight);
+    result:=recycler^.newListLiteral()^.appendInt(recycler,plotWidth)^.appendInt(recycler,plotHeight);
     disposeMessage(closedRequest);
   end else result:=nil; end;
 
