610e97438a33ad103b6a046eee87b5424b1be526
Martin Schlegel <m.schlegel81@googlemail.com>
2024-09-04 19:28:38 +0200

    Fixed build bootstrapping

--- make.bat
+++ make.bat
@@ -1,8 +1,11 @@
 @rem You may have to edit the path to lazbuild.exe to make this work...
 
 @if not exist core\res_version.inc echo CODE_HASH='x'; BUILD_NUMBER=1; VERSION='0.0.0'; > core\res_version.inc
-@if not exist target\Win64\mnh_light.exe (
+@if not exist target\Win64\mnh.exe (
   ..\lazarus64\lazbuild.exe -B --bm=deployment consoles\mnh_light.lpi
-  move consoles\mnh_light.exe mnh_temp.exe)  
-@copy target\Win64\mnh_light.exe mnh_temp.exe
+  mkdir target
+  mkdir target\Win64
+  move consoles\mnh_light.exe target\Win64\mnh_light.exe
+  target\Win64\mnh_light.exe make.mnh target\Win64\mnh.exe)
+@copy target\Win64\mnh.exe mnh_temp.exe
 @mnh_temp.exe make.mnh %* && del mnh_temp.exe
--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ ! -f core/res_version.inc ]; then echo "CODE_HASH='x'; BUILD_NUMBER=1; VERSION='0.0.0';" > core/res_version.inc;fi
-if [ ! -f target/Linux/mnh_light ]; then
+if [ ! -f target/Linux/mnh ]; then
   lazbuild -B --bm=deployment consoles/mnh_light.lpi
-  mv consoles/mnh_light mnh_tmp
-else 
-  cp target/Linux/mnh_light mnh_tmp 
+  mkdir target
+  mkdir target/Linux
+  mv consoles/mnh_light target/Linux/mnh_light
+  target/Linux/mnh_light make.mnh target/Linux/mnh
 fi
+cp target/Linux/mnh mnh_tmp
 ./mnh_tmp make.mnh $@
 rm mnh_tmp
